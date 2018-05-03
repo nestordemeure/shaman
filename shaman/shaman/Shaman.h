143ce09c7aa06c7fed4cca0571fab5785e0958b8
Nestor DEMEURE <nestor.demeure.ocre@cea.fr>
2018-05-03 11:54:28 +0200

    added all missing openmp reductions

--- shaman/shaman/Shaman.h
+++ shaman/shaman/Shaman.h
@@ -171,16 +171,36 @@ using Slong_double = S<long double, long double, long double>;
 #endif //NO_SHAMAN
 
 //-------------------------------------------------------------------------------------------------
-// OPENMP
+// OPENMP (require openMP 4.0+ to get reductions on user defined types)
 
 #ifdef _OPENMP
-// require openMP 4.0+ to get reductions on user defined types
-#pragma omp declare reduction(+:Sfloat : omp_out=omp_in+omp_out)    initializer(omp_priv=Sfloat(0.f))
-#pragma omp declare reduction(+:Sdouble: omp_out=omp_in+omp_out)	initializer(omp_priv=Sdouble(0.))
-#pragma omp declare reduction(-:Sfloat : omp_out=omp_in+omp_out)	initializer(omp_priv=Sfloat(0.f))
-#pragma omp declare reduction(-:Sdouble: omp_out=omp_in+omp_out)	initializer(omp_priv=Sdouble(0.))
-#pragma omp declare reduction(*:Sfloat : omp_out=omp_in*omp_out)	initializer(omp_priv=Sfloat(1.f))
-#pragma omp declare reduction(*:Sdouble: omp_out=omp_in*omp_out)	initializer(omp_priv=Sdouble(1.))
+#include <limits>
+
+// +
+#pragma omp declare reduction(+:Sfloat : omp_out=omp_in+omp_out)        initializer(omp_priv=Sfloat(0.f))
+#pragma omp declare reduction(+:Sdouble: omp_out=omp_in+omp_out)	    initializer(omp_priv=Sdouble(0.))
+#pragma omp declare reduction(+:Slong_double: omp_out=omp_in+omp_out)	initializer(omp_priv=Slong_double(0.L))
+
+// -
+#pragma omp declare reduction(-:Sfloat : omp_out=omp_in+omp_out)	    initializer(omp_priv=Sfloat(0.f))
+#pragma omp declare reduction(-:Sdouble: omp_out=omp_in+omp_out)	    initializer(omp_priv=Sdouble(0.))
+#pragma omp declare reduction(-:Slong_double: omp_out=omp_in+omp_out)	initializer(omp_priv=Slong_double(0.L))
+
+// *
+#pragma omp declare reduction(*:Sfloat : omp_out=omp_in*omp_out)	    initializer(omp_priv=Sfloat(1.f))
+#pragma omp declare reduction(*:Sdouble: omp_out=omp_in*omp_out)    	initializer(omp_priv=Sdouble(1.))
+#pragma omp declare reduction(*:Slong_double: omp_out=omp_in*omp_out)	initializer(omp_priv=Slong_double(1.L))
+
+// max
+#pragma omp declare reduction(max:Sfloat : omp_out=max(omp_in,omp_out))	        initializer(omp_priv=Sfloat(std::numeric_limits<float>::min()))
+#pragma omp declare reduction(max:Sdouble : omp_out=max(omp_in,omp_out))        initializer(omp_priv=Sdouble(std::numeric_limits<double>::min()))
+#pragma omp declare reduction(max:Slong_double : omp_out=max(omp_in,omp_out))   initializer(omp_priv=Slong_double(std::numeric_limits<long double>::min()))
+
+// min
+#pragma omp declare reduction(min:Sfloat : omp_out=min(omp_in,omp_out))	        initializer(omp_priv=Sfloat(std::numeric_limits<float>::max()))
+#pragma omp declare reduction(min:Sdouble : omp_out=min(omp_in,omp_out))        initializer(omp_priv=Sdouble(std::numeric_limits<double>::max()))
+#pragma omp declare reduction(min:Slong_double : omp_out=min(omp_in,omp_out))   initializer(omp_priv=Slong_double(std::numeric_limits<long double>::max()))
+
 #endif //_OPENMP
 
 //-------------------------------------------------------------------------------------------------
