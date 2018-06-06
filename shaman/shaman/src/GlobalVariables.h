c56c46dec77aecfe75269f08721d9886ac18b74b
Nestor DEMEURE <nestor.demeure.ocre@cea.fr>
2018-06-06 16:45:26 +0200

    gettign away from header only

--- shaman/shaman/src/GlobalVariables.h
+++ shaman/shaman/src/GlobalVariables.h
@@ -9,16 +9,20 @@
  * global constants
  */
 #include <stack>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <atomic>
 
 namespace Shaman
 {
     // used to model the stacktrace
     using Tag = unsigned short int; // represents a block
-    thread_local static std::vector<std::string> tagDecryptor({"integer_cast", "main"}); // array that associate with tags (indexes) with block-names
-    static Tag tagIntegerCast = 0;
-    static Tag tagMain = 1;
-    thread_local static std::unordered_map<std::string, Tag> nameEncryptor({{"integer_cast", tagIntegerCast}, {"main", tagMain}}); // hashtable that associate block-names with tags
-    thread_local static std::stack<Tag> tagStack({tagMain}); // contains the current stack
+    thread_local static std::vector<std::string> tagDecryptor; // array that associate with tags (indexes) with block-names
+    static const Tag tagIntegerCast = 0;
+    static const Tag tagMain = 1;
+    thread_local static std::unordered_map<std::string, Tag> nameEncryptor; // hashtable that associate block-names with tags
+    thread_local static std::stack<Tag> tagStack; // contains the current stack
 
     // counter for the number of unstable branches
     static std::atomic_int unstableBranchCounter;
