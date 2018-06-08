2ecdc8f8bcdbb39c4cf6284eeb07876e83fc9e2c
Nestor DEMEURE <nestor.demeure.ocre@cea.fr>
2018-06-08 13:59:34 +0200

    made name->tag threadssafe, removed useless todo from fucntions

--- shaman/shaman/src/GlobalVar.cpp
+++ shaman/shaman/src/GlobalVar.cpp
@@ -10,6 +10,7 @@ const Tag ShamanGlobals::tagMain = 1;
 std::vector<std::string> ShamanGlobals::tagDecryptor = std::vector<std::string>({"integer_cast", "main"}); // array that associate with tags (indexes) with block-names
 std::unordered_map<std::string, Tag> ShamanGlobals::nameEncryptor = {{"integer_cast", ShamanGlobals::tagIntegerCast}, {"main", ShamanGlobals::tagMain}}; // hashtable that associate block-names with tags
 thread_local std::stack<Tag> ShamanGlobals::tagStack({ShamanGlobals::tagMain}); // contains the current stack
+std::mutex ShamanGlobals::mutexAddName;
 
 // counter for the number of unstable branches
 std::atomic_int ShamanGlobals::unstableBranchCounter;
--- shaman/shaman/src/GlobalVar.h
+++ shaman/shaman/src/GlobalVar.h
@@ -10,6 +10,7 @@
 #include <vector>
 #include <unordered_map>
 #include <atomic>
+#include <mutex>
 
 using Tag = unsigned short int; // represents a block
 
@@ -17,11 +18,12 @@ class ShamanGlobals
 {
 public:
     // used to model the stacktrace
-    static std::vector<std::string> tagDecryptor; // array that associate with tags (indexes) with block-names // TODO make thread safe
+    static std::vector<std::string> tagDecryptor; // array that associate with tags (indexes) with block-names
     static const Tag tagIntegerCast;
     static const Tag tagMain;
-    static std::unordered_map<std::string, Tag> nameEncryptor; // hashtable that associate block-names with tags // TODO make thread safe
+    static std::unordered_map<std::string, Tag> nameEncryptor; // hashtable that associate block-names with tags
     thread_local static std::stack<Tag> tagStack; // contains the current stack
+    static std::mutex mutexAddName; // guards against concurent addition of names in the encryptor/decryptor
 
     // counter for the number of unstable branches
     static std::atomic_int unstableBranchCounter;
