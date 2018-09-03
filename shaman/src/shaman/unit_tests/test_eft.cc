d633e382079f68dce899a783a9dec3be6cbcf2e4
Nestor DEMEURE <nestor.demeure.ocre@cea.fr>
2018-09-03 11:08:18 +0200

    added test for cancelation and exact substraction

--- shaman/src/shaman/unit_tests/test_eft.cc
+++ shaman/src/shaman/unit_tests/test_eft.cc
@@ -1,30 +1,58 @@
-#include <utility> // for std::swap
 #include "../eft.h"
 
 #include <gtest/gtest.h>
 
-namespace {
 /*
- * small test to check whether the EFT for the sum is computed accurately
- * TODO : this test pass even with -Ofast -ffast-math
+ * NOTE avoid power of two in tests, they tend to produce correct roundings a lot more often
  */
-template<typename F>
-bool test_eft(F x, F y) {
-//    F x = 1e30;
-//    F y = 1.f;
-    F z = x + y;
-    F error = EFT::TwoSum(x, y, z);
+namespace
+{
+    /*
+     * test whether an EFT is computed accurately in case of cancellation
+     * x is much larger than y
+     */
+    template<typename F>
+    bool test_cancellation(F x, F y)
+    {
+        F z = x + y;
+        F error = EFT::TwoSum(x, y, z);
+        return (error == y);
+    }
 
-    return (error == y);
-}
+    /*
+     * test whether an EFT is computed accurately in case of exact subtraction
+     * y/2 < x < y*2
+     * (requires larger bound to make sure that rounding will not get lucky)
+     */
+    template<typename F>
+    bool test_exactSub(F x, F y)
+    {
+        F z = x - y;
+        F error = EFT::TwoSum(x, -y, z);
+        return (error == 0);
+    }
 
-TEST(EFT, float) {
-    EXPECT_TRUE(test_eft<float>(1e30, 1.f));
-}
+    TEST(EFT, float)
+    {
+        // cancellation
+        EXPECT_TRUE(test_cancellation<float>(1e30, 1.1));
+        EXPECT_FALSE(test_cancellation<float>(1.2, 1.2));
+        // exact subtraction
+        EXPECT_TRUE(test_exactSub<float>(1.3, 0.7));
+        EXPECT_TRUE(test_exactSub<float>(1.4, 2.1));
+        EXPECT_FALSE(test_exactSub<float>(1.5, 0.3));
+        EXPECT_FALSE(test_exactSub<float>(1.7, 4.7));
+    }
 
-TEST(EFT, double) {
-  EXPECT_TRUE(test_eft<double>(1e30, 1.));
-  EXPECT_FALSE(test_eft<double>(1., 1.));
-}
-
+    TEST(EFT, double)
+    {
+        // cancellation
+        EXPECT_TRUE(test_cancellation<double>(1e30, 1.1));
+        EXPECT_FALSE(test_cancellation<double>(1.2, 1.2));
+        // exact subtraction
+        EXPECT_TRUE(test_exactSub<double>(1.3, 0.7));
+        EXPECT_TRUE(test_exactSub<double>(1.4, 2.1));
+        EXPECT_FALSE(test_exactSub<double>(1.5, 0.3));
+        EXPECT_FALSE(test_exactSub<double>(1.7, 4.7));
+    }
 }
