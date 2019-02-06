31221b13f1927fb9c81abee60cc27f1c7585ce03
Nestor DEMEURE <nestor.demeure.ocre@cea.fr>
2019-02-06 10:08:17 +0100

    made sure that error will be set in input stream operator

--- shaman/src/shaman/methods.h
+++ shaman/src/shaman/methods.h
@@ -187,8 +187,8 @@ templated std::istream& operator>>(std::istream& is, Snum& n)
 
     // modifies the Snum in place
     n.number = num;
-    /*n.error = 0;
-    n.errorComposants = Serror(0);*/
+    n.error = 0;
+    n.errorComposants = Serror();
 
     return is;
 }
