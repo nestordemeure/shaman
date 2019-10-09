283e18bbec74176b76c9b712eef77faaf28e5f6c
Nestor DEMEURE <nestor.demeure.ocre@cea.fr>
2019-10-09 14:41:18 +0200

    added single file to load all traits

---
+++ shaman/src/shaman/helpers/Shaman_trilinos.h
@@ -0,0 +1,8 @@
+#pragma  once
+
+#include "trilinos/arithmeticTraits_kokkos.h"
+#include "trilinos/lapackTraits_belosTeuchos.h"
+#include "trilinos/reductionTraits_kokkos.h"
+#include "trilinos/scalarTraits_teuchos.h"
+#include "trilinos/serializationTraits_teuchos.h"
+
