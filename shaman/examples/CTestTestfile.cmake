72d7200ca9fa5545f05293a3acceab7cd35729e0
Nestor DEMEURE <nestor.demeure.ocre@cea.fr>
2019-11-08 10:45:54 +0100

    removed useless surfolder

--- shaman/examples/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /ccc/home/cont001/ocre/demeuren/thesis/code/shaman/shaman/examples
-# Build directory: /ccc/home/cont001/ocre/demeuren/thesis/code/shaman/shaman/examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("schrodinger")
-subdirs("cadna")
-subdirs("performances")
--- shaman/examples/schrodinger/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /ccc/home/cont001/ocre/demeuren/thesis/code/shaman/shaman/examples/schrodinger
-# Build directory: /ccc/home/cont001/ocre/demeuren/thesis/code/shaman/shaman/examples/schrodinger
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
