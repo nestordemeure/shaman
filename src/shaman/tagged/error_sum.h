667e261d96edbfa3b98c9a04956c7cfbc4f26c18
Nestor Demeure <nestordemeure@gmail.com>
2021-02-09 13:56:46 +0100

    fixed potential division by zero in display of tagged error

--- src/shaman/tagged/error_sum.h
+++ src/shaman/tagged/error_sum.h
@@ -189,6 +189,12 @@ public:
         // rectifies the sign of the sum of absolute errors to match the sign of the sum
         totalAbsoluteError = std::copysign(totalAbsoluteError, totalError);
 
+        // avoid divide-by-zero by returning early if all terms are 0
+        if (totalAbsoluteError == 0.)
+        {
+            return "[]";
+        }
+
         // collects the relevant data expressed in percent of the total error
         std::vector<std::pair<Tag, errorType>> data;
         bool droppedNonSignificantTerms = false;
